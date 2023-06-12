291056ed58ba890f188b7d1368dc54f593e497a2
VJS <minsung_96@naver.com>
2023-06-12 23:59:49 +0900

    Modal Props

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -57,10 +57,25 @@ const Modal: React.FC<ModalProps> = ({
     if(!isOpen) return null
     
     return (
-        <div>
-            Modal
+        <>
+            <div
+                className="
+                    justify-center
+                    items-center
+                    flex
+                    overflow-x-hidden
+                    overflow-y-auto
+                    fixed
+                    inset-0
+                    z-50
+                    outline-none
+                    focus:outline-none
+                    bg-neutral-800/70
+                "
+            >
 
-        </div>
+            </div>
+        </>
     )
 }
 
