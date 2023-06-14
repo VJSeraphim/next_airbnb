ef29ccc9714aa5462a400ab69f7bfc4e353f4510
VJS <minsung_96@naver.com>
2023-06-14 23:57:03 +0900

    Authentication Modal Init

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { IconType } from "react-icons"
 
 interface ButtonProps {
@@ -6,7 +8,7 @@ interface ButtonProps {
     disabled?: boolean
     outline?: boolean
     small?: boolean
-    icon?: boolean
+    icon?: IconType
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -15,12 +17,41 @@ const Button: React.FC<ButtonProps> = ({
     disabled,
     outline,
     small,
-    icon
+    icon: Icon
 }) => {
     return (
-        <div>
+        <button
+            onClick={onClick}
+            disabled={disabled}
+            className={`
+                relative
+                disabled:opacity-70
+                disabled:cursor-not-allowed
+                transition
+                rounded-lg
+                hover:opacity-80
+                w-full
+                ${outline ? 'bg-white' : 'bg-rose-500'}
+                ${outline ? 'border-black' : 'border-rose-500'}
+                ${outline ? 'text-black' : 'text-white'}
+                ${small ? 'py-1' : 'py-3'}
+                ${small ? 'text-sm' : 'text-md'}
+                ${small ? 'font-light' : 'font-semibold'}
+                ${small ? 'border-[1px]' : 'border-2'}
+            `}
+        >
+            {Icon && (
+                <Icon 
+                    size={24}
+                    className="
+                        absolute
+                        left-4
+                        top-3
+                    "
+                />
+            )}
             {label}
-        </div>
+        </button>
     )
 }
 
