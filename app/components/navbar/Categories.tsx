88d720c73b5506aa65ee9d6fb8af437ee781717d
VJS <minsung_96@naver.com>
2023-07-05 23:59:15 +0900

    Category Box Component

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,8 +3,9 @@ import Container from "../Container"
 import { TbBeach } from 'react-icons/tb'
 import { GiWindmill } from 'react-icons/gi'
 import { MdOutlineVilla } from 'react-icons/md'
+import CategoryBox from "../CategoryBox"
 
-export const categoris=[
+export const categories = [
     {
         label: 'Beach',
         icon: TbBeach,
@@ -35,7 +36,14 @@ const Categories = () => {
                 overflow-x-auto
             "
         >
-
+            {categories.map((item) => (
+                <CategoryBox 
+                    key={item.label}
+                    label={item.label}
+                    description={item.description}
+                    icon={item.icon}
+                />
+            ))}
         </div>
     </Container>
   )
