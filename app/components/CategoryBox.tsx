10d684ac80e87799d44cdcc76f6941a387e7a991
VJS <minsung_96@naver.com>
2023-07-06 23:57:38 +0900

    Category Constants

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -41,7 +41,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
         })
 
         router.push(url)
-    }, [])
+    }, [label, params, router])
 
     return (
         <div
@@ -59,6 +59,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
                 ${selected ? 'border-b-neutral-800' : 'border-transparent'}
                 ${selected ? 'text-neutral-800' : 'text-neutral-500'}
             `}
+            onClick={handleClick}
         >
             <Icon size={26} />
             <div className="font-medium text-sm">
