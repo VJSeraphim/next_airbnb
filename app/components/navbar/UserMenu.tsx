ab0264a6e6991cb344e235b052ee9974a84d6c72
VJS <minsung_96@naver.com>
2023-08-21 23:58:43 +0900

    Home Listing Debug + Properties Route

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -112,7 +112,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                                     label="Reservations"
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
+                                    onClick={() => router.push('/properties')}
                                     label="Properties"
                                 />
                                 <MenuItem
